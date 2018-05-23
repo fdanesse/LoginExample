0db6cb0f085e8fb46a9f876f3c3c08f20505a741
flavio <fdanesse@gmail.com>
2018-05-22 22:03:28 -0300

    LoginExample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LoginExample';
+
+  private authSubscription: Subscription;
+  public user;
+
+  constructor (public afAuth: AngularFireAuth) {
+    // subscription to observer
+    this.authSubscription = this.obs()
+      .subscribe(user => {
+        this.user = user;
+        });
+  }
+
+  obs(): Observable<firebase.User> {
+    // Observer login and logout
+    return this.afAuth.authState;
+  }
+
+  login() {
+    // popup google login
+    return this.afAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    // logout
+    return this.afAuth.auth.signOut();
+  }
 }
